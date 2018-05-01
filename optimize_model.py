d0b207cca96c1154037b10dc190ac93cbba82e14
Alexander Zarichkovyi <ALEXKIRNAS@users.noreply.github.com>
2018-05-01 17:15:24 +0300

    Change project structure. Add model optimizer script.

---
+++ optimize_model.py
@@ -0,0 +1,88 @@
+import os
+import click
+import tempfile
+
+import tensorflow as tf
+
+from utils import graph_utils
+
+
+@click.command()
+@click.option('--model_ckpt_path',
+              help='Path to trained model.',
+              default='./model/final.ckpt')
+@click.option('--input_node_name',
+              help='Name of input node.',
+              default='content')
+@click.option('--output_node_name',
+              help='Name of output node.',
+              default='mul_37')
+@click.option('--desired_input_shape',
+              help='Comma separated input shape.',
+              default='1,360,640,3')
+@click.option('--output_model_path',
+              help='Path where to save optimized model.',
+              default='./model/optimized_model.pb')
+def main(model_ckpt_path: str,
+         input_node_name: str,
+         output_node_name: str,
+         desired_input_shape: str,
+         output_model_path: str):
+
+    desired_input_shape = [
+        int(value)
+        for value in desired_input_shape.split(',')
+    ]
+
+    if len(desired_input_shape) != 4:
+        raise ValueError('Input shape must be 4 dimensional.')
+
+    sess = tf.Session()
+
+    graph_utils.create_transformation_network(
+        sess=sess,
+        model_ckpt_path=model_ckpt_path,
+        input_node_name=input_node_name,
+        desired_input_shape=desired_input_shape
+    )
+
+    tmp_model_prefix = 'model'
+    save_folder = tempfile.gettempdir()
+    save_path = os.path.join(save_folder, tmp_model_prefix)
+
+    saver = tf.train.Saver()
+    saver.save(sess=sess, save_path=save_path, global_step=0)
+
+    tf.reset_default_graph()
+
+    graph_utils.load_model_from_ckpt(
+        sess=sess,
+        model_path=save_path + '-0'
+    )
+
+    frozen_graph_def = graph_utils.freeze_graph(
+        sess=sess,
+        output_node_name=output_node_name
+    )
+
+    optimized_graph_def = graph_utils.optimize_graphdef(
+        input_graph_def=frozen_graph_def,
+        input_node_name=input_node_name,
+        output_node_name=output_node_name
+    )
+
+    save_model_dir = os.path.dirname(output_model_path)
+
+    if not os.path.exists(save_model_dir):
+        os.makedirs(save_model_dir)
+
+    tf.train.write_graph(
+        graph_or_graph_def=optimized_graph_def,
+        logdir='.',
+        name=output_model_path,
+        as_text=False
+    )
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/graph_utils.py
@@ -0,0 +1,101 @@
+from typing import Iterable
+
+import tensorflow as tf
+from tensorflow.tools.graph_transforms import TransformGraph
+
+from transform import Transform
+
+
+def load_model_from_ckpt(sess: tf.Session,
+                         model_path: str) -> tf.Session:
+    """
+    Load model from checkpoint.
+    :param sess: Active TensorFlow session.
+    :param model_path: model checkpoint path.
+    :return: Active TensorFlow session containing the variables.
+    """
+
+    saver = tf.train.import_meta_graph(model_path + '.meta', clear_devices=True)
+    saver.restore(sess, model_path)
+
+    return sess
+
+
+def freeze_graph(sess: tf.Session,
+                 output_node_name: str) -> tf.Graph:
+    """
+    Extract the sub graph defined by the output nodes and convert
+    all its variables into constant
+    :param sess: Active TensorFlow session containing the variables.
+    :param output_node_name: name of the result node in the graph.
+    :return: GraphDef containing a simplified version of the original.
+    """
+
+    output_graph_def = tf.graph_util.convert_variables_to_constants(
+        sess=sess,
+        input_graph_def=tf.get_default_graph().as_graph_def(),
+        output_node_names=[output_node_name]
+    )
+
+    return output_graph_def
+
+
+def optimize_graphdef(input_graph_def: tf.GraphDef,
+                      input_node_name: str,
+                      output_node_name: str) -> tf.GraphDef:
+    """
+    Optimize input GraphDef.
+    :param input_graph_def: GraphDef containing a network.
+    :param input_node_name: GraphDef input node name.
+    :param output_node_name: GraphDef output node name.
+    :return: GraphDef containing a optimized version of the original.
+    """
+
+    optimized_graph_def = TransformGraph(
+        input_graph_def=input_graph_def,
+        inputs=[input_node_name],
+        outputs=[output_node_name],
+        transforms=["merge_duplicate_nodes",
+                    "strip_unused_nodes",
+                    "remove_device",
+                    "fold_constants",
+                    "flatten_atrous_conv",
+                    "fold_batch_norms",
+                    "fold_old_batch_norms",
+                    "fuse_pad_and_conv",
+                    "fuse_resize_pad_and_conv",
+                    "sort_by_execution_order"]
+    )
+
+    return optimized_graph_def
+
+
+# TODO: move to networks folder.
+def create_transformation_network(
+        sess: tf.Session,
+        model_ckpt_path: str,
+        input_node_name: str,
+        desired_input_shape: Iterable[int]) -> tf.Graph:
+    """
+    Create transformation network from checkpoint.
+    :param sess: Active TensorFlow session.
+    :param model_ckpt_path: path where model checkpoint are stored.
+    :param input_node_name: model input node name.
+    :param desired_input_shape: transformation input shape.
+    :return: tf.Graph with loaded model.
+    """
+    img_placeholder = tf.placeholder(tf.float32,
+                                     shape=desired_input_shape,
+                                     name=input_node_name)
+
+    transformation_network = Transform()
+    transformation_network.net(img_placeholder)
+
+    saver = tf.train.Saver()
+    try:
+        saver.restore(sess, model_ckpt_path)
+    except:
+        raise RuntimeError('Something wrong with checkpoint. '
+                           'Check checkpoint path.')
+
+    return sess.graph
